b189c93ef85a318768007d3982638e75a79e6e71
yinyang513 <zyingyip@gmail.com>
2021-07-08 03:08:26 -0400

    added embedded links

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
-# controlf1
+# controlf
 
 To get the website running, open a terminal and do the following:
 (make sure your Python version is 3.6+)
-	1. enter the controlf folder 'cd path/to/controlf'
-	2. install all necessary packages 'pip3 install -r requirements.txt' (or use pip3)
-	3. export flask (Mac, Linux)'export FLASK_APP=app.py', (Windows)'set FLASK_APP=app.py'
-	4. back to the terminal, run the app 'python3 -m flask run' (or use python3)
-	5. open your browser, and open the local website 'localhost:5000'
+1. enter the controlf folder `cd path/to/controlf`
+2. install all necessary packages `pip3 install -r requirements.txt` (or use pip3)
+3. export flask (Mac, Linux)`export FLASK_APP=app.py`, (Windows)'set FLASK_APP=app.py'
+4. back to the terminal, run the app `python3 -m flask run` (or use python3)
+5. open your browser, and open the local website 'localhost:5000'
+
+<!-- 1. switch to yelp-env using `. yelp-env/bin/activate`
+2. install all dependencies `pip install -r requirements.txt`
+3. `export FLASK_APP=app.py`
+4. run the backend `flask run`
+5. switch the frontend folder `cd yelp-app-frontend`
+6. run the frontend `npm start` -->
--- app.py
+++ app.py
@@ -7,29 +7,32 @@ app = Flask(__name__)
 def getWordAndURL():
     if request.method == 'POST':
         word = request.form.get('word')
-        url = request.form.get('url')
+        url = request.form.get('utube') # include mp4 videos and implement with speech to text api
 
         yt = pytube.YouTube(url)
-        subt = yt.captions.get_by_language_code("en")
+        subt = yt.captions.get_by_language_code("en") #add more caption languages
         sub = subt.generate_srt_captions().lower()
         subtitles = sub.split('\n')
         for x in range(len(subtitles)):
             subtitles[x] = subtitles[x].split(" ")
         time_stamps = []
-        for x in range(len(subtitles)):
+        for x in range(len(subtitles)): # finding word
             for j in range(len(subtitles[x])):
                 if subtitles[x][j] == word:
                     time_stamps.append(subtitles[x - 1][0][:-4])
         if not time_stamps:
-            # print("Sorry! We could not find your word. :(")
-            return render_template('controlf.html', name = 'Sorry! We could not find your word. :(')
+            return render_template('controlf.html', name = 'Sorry! We could not find your word. :(', back = True)
         else:
-            # print("Here are your time stamps:")
-            # print('\n'.join(list(dict.fromkeys(time_stamps))))
+            links = []
             timestamps = list(dict.fromkeys(time_stamps))
-            return render_template('controlf.html', name = 'Here are your time stamps:', timestamps = timestamps)
+            for time in timestamps:
+                split_time = time.split(':')
+                second = int(split_time[0])*3600 + int(split_time[1])*60 + int(split_time[2])
+                link_timestamps = url + "&t=" + str(second) + "s"
+                links.append(link_timestamps)
+            return render_template('controlf.html', name = 'Here are your time stamps:', links_timestamps = zip(links,timestamps), back = True)
     else:
         return render_template('controlf.html',form = True)
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=True) 
